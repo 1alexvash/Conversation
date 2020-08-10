a6ffe37fe45778a329e878051a74939d8675cabd
Alexander <1alexvash@gmail.com>
2020-08-10 10:15:45 +0300

    Updates

---
+++ precache-manifest.54f06d8e0a2f0b877b0616651498719d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e304ab0215a1c246f5d8abeb2f5c7738",
+    "url": "/Conversation/index.html"
+  },
+  {
+    "revision": "6ef83749c3c4b6b0e896",
+    "url": "/Conversation/static/css/main.c110bca6.chunk.css"
+  },
+  {
+    "revision": "2c8edf5918a359a7574a",
+    "url": "/Conversation/static/js/2.33e964e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Conversation/static/js/2.33e964e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ef83749c3c4b6b0e896",
+    "url": "/Conversation/static/js/main.c6b7f31f.chunk.js"
+  },
+  {
+    "revision": "4a240a8172fc97631d86",
+    "url": "/Conversation/static/js/runtime-main.573e3ff6.js"
+  },
+  {
+    "revision": "e575b24fbd8811371cd227548a9931d9",
+    "url": "/Conversation/static/media/conversation.e575b24f.png"
+  },
+  {
+    "revision": "7ffd03ad81136936cf36fe52510d1fcc",
+    "url": "/Conversation/static/media/random.7ffd03ad.png"
+  },
+  {
+    "revision": "d530b96927421ea416db482e28d742d1",
+    "url": "/Conversation/static/media/random2.d530b969.png"
+  }
+]);
