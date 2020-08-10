4885c3d08c87b268215e85c47899fbec51614da3
Alexander <1alexvash@gmail.com>
2020-08-10 10:30:59 +0300

    discussional page has a "get random word" button too

--- src/components/Discussional/Discussional.js
+++ src/components/Discussional/Discussional.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "./data.json";
 
 import randomWordImg from "../../images/random2.png";
 
 const Discussional = () => {
-  const generateRandomWord = () => {};
+  const [chosenWord, setChosenWord] = useState();
+  const [hideQuestion, setHideQuestion] = useState(false);
+
+  const generateRandomWord = () => {
+    const random = Math.floor(Math.random() * data.length);
+    setChosenWord(random);
+    setHideQuestion(true);
+    setTimeout(() => {
+      setHideQuestion(false);
+    }, 2000);
+  };
 
   const RandomWordComponent = (
     <img
       src={randomWordImg}
-      className="random-word"
+      className={`random-word ${hideQuestion ? "hide" : ""}`}
       onClick={generateRandomWord}
       title="Get a random word"
       alt=""
@@ -18,8 +28,10 @@ const Discussional = () => {
 
   return (
     <div className="Discussional">
-      {data.map((q) => (
-        <p key={q}>{q}</p>
+      {data.map((q, index) => (
+        <p key={q} className={index === chosenWord && "chosen"}>
+          {q}
+        </p>
       ))}
       {RandomWordComponent}
     </div>
