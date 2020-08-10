b8482ba769c2e841b432944e96c8a809ebe8c7ec
Alexander <1alexvash@gmail.com>
2020-08-10 10:17:32 +0300

    updated with hashrouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./scss/main.css";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 import Menu from "./components/Menu/Menu";
 import Alphabetical from "./components/Alphabetical/Alphabetical";
