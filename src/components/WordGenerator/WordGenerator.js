dda0a353de4e69955059a3ddf6293dfc42b59bdb
Alexander <1alexvash@gmail.com>
2020-08-10 18:03:00 +0300

    Added 3rd page and spinner component

--- src/components/WordGenerator/WordGenerator.js
+++ src/components/WordGenerator/WordGenerator.js
@@ -1,20 +1,39 @@
 import React, { useState } from "react";
+import Spinner from "../Spinner/Spinner";
 
 import data from "./data.json";
 
 const WordGenerator = () => {
-  const [randomWord, setRandomWord] = useState("XXX");
+  const [randomWord, setRandomWord] = useState("");
+  const [animationProgress, setAnimationProgress] = useState(false);
 
   const generateWord = () => {
     const random = Math.floor(Math.random() * data.length);
     const randomWord = data[random];
     setRandomWord(randomWord);
+
+    // Animation
+    setAnimationProgress(true);
+    const randomTime = 500 + Math.round(Math.random() * 6) * 250;
+    setTimeout(() => {
+      setAnimationProgress(false);
+    }, randomTime);
   };
 
+  if (animationProgress) {
+    return (
+      <div className="fix-content-to-center">
+        <Spinner />
+      </div>
+    );
+  }
+
   return (
-    <div className="Word-Generator">
-      random word: {randomWord}
-      <button onClick={generateWord}>Generate a word</button>
+    <div className="Word-Generator fix-content-to-center">
+      <div className="random-word">{randomWord}</div>
+      <button className="get-word" onClick={generateWord}>
+        {!randomWord ? "Generate a word" : "Generate a new word"}
+      </button>
     </div>
   );
 };
